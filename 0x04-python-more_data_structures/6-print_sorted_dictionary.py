b68fe9c02aa28c299ba7b30e971fb641d08d5f05
bemadm <bemnetadmasu9@gmail.com>
2022-09-30 19:35:56 +0000

    tasks done

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a functionn that prints a
+
+dictionary by ordered keys"""
+
+
+
+def print_sorted_dictionary(a_dictionary):
+
+    for key in sorted(list(a_dictionary.keys())):
+
+        print(f"{key}: {a_dictionary.get(key)}")
