5d9168d4f1181a84a98d9ab58b99694f6c3ef0c8
bemadm <bemnetadmasu9@gmail.com>
2022-09-30 19:49:06 +0000

    tasks done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
-
-
 """a function that returns a set of all
-
 elements present in only one set"""
 
 
-
-
-
 def only_diff_elements(set_1, set_2):
-
     return set_1 ^ set_2
 
 
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""a functionn that prints a
 
+"""a functionn that prints a
 dictionary by ordered keys"""
 
 
-
 def print_sorted_dictionary(a_dictionary):
-
     for key in sorted(list(a_dictionary.keys())):
-
         print(f"{key}: {a_dictionary.get(key)}")
