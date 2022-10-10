d4df8606cf85ef357c2476f21c4d42e47655a2a0
bemadm <bemnetadmasu9@gmail.com>
2022-10-10 03:04:45 -0700

    task1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,28 @@
+
+n that prints x elements of a list."""
+
+
+
+
+
+def safe_print_list(my_list=[], x=0):
+
+        count = 0
+
+            try:
+
+                    for i in range(0, x):
+
+                                print(my_list[i], end='')
+
+                                            count += 1
+
+                                                except Exception:
+
+                                                        error = Exception
+
+                                                            finally:
+
+                                                                    print('')
+
+                                                                        return count
