45adc644c43dd1b6310df79f7ed46e6a62a185ad
bemadm <bemnetadmasu9@gmail.com>
2022-10-10 05:10:52 -0700

    fix task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(0, x):
-        print(my_list[i], end='')
-        count += 1
+            print(my_list[i], end='')
+            count += 1
     except Exception:
         error = Exception
     finally:
