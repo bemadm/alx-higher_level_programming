9ac55c66741cb95477632057e6d3c484d7be5d08
bemadm <bemnetadmasu9@gmail.com>
2022-10-10 03:57:58 -0700

    tasks 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 
-n that prints x elements of a list."""
+
+
+"""a function that prints x elements of a list."""
 
 
 
@@ -11,18 +14,18 @@ def safe_print_list(my_list=[], x=0):
 
             try:
 
-                    for i in range(0, x):
+                        for i in range(0, x):
 
-                                print(my_list[i], end='')
+                                        print(my_list[i], end='')
 
-                                            count += 1
+                                                    count += 1
 
-                                                except Exception:
+                                                        except Exception:
 
-                                                        error = Exception
+                                                                    error = Exception
 
-                                                            finally:
+                                                                        finally:
 
-                                                                    print('')
+                                                                                    print('')
 
-                                                                        return count
+                                                                                        return count
